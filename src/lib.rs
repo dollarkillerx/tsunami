29b50b9aa71960497d45b32a6cba831aa00f15fc
dollarkiller <wangy@mvalley.io>
2020-06-23 18:56:17 +0800

    add core_dns

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
 pub mod config;
 pub mod core_dns;
+
+#[cfg(Test)]
+mod test {
+    use super::*;
+    #[test]
+    fn lookup_ns() {
+        match core_dns::lookup_ns("www.dollarkiller.com") {
+            Ok(_data) => {},
+            Err(e) => panic!("err: ",e),
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use tsunami::*;
+
 fn main() {
+    match core_dns::lookup_ns("www.dollarkiller.com") {
+        Err(e) => panic!(format!("err: {}",e)),
+        Ok(_) => {},
+    };
     println!("Hello, world!");
 }
